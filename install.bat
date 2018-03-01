f6b85cb20b38d4225e6dd497d07504cb9e4fd3cd
sdkn104 <sdkn104@yahoo.co.jp>
2018-03-02 07:31:55 +0900

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+
+del  /F "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp\PCä«óùóp.vbs"
+copy /Y "%~dp0"PCä«óùóp.vbs "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp"
+
+pause
+
