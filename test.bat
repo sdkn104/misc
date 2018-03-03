c37d6c0139f85ddf6c3029c2916b69aaea592768
sdkn104 <sdkn104@yahoo.co.jp>
2018-03-03 20:55:38 +0900

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,28 +1,35 @@
 
+FOR /F "delims=^^^" %%i in ('ver') do set V=%%i
+echo %DATE%,%TIME%,%COMPUTERNAME%,%USERNAME%,OSVERSION,%V%
+
+REM --- install -----------
 net use
 net use /PERSISTENT:NO
-net use Y: \\192.168.1.100\share\sadakane\HOKAN\その他
+net use N: \\192.168.1.100\share\sadakane\HOKAN\その他
 net use
-dir Y:
+cd /d N:\
+install.bat
 
-
-FOR /F "delims=^^^" %%i in ('ver') do set VVV=%%i
-echo %VVV%
-
+REM --- user account ----
 net user
 net user Toshiyuki
 net localgroup
 net localgroup Administrators
 net localgroup Administrators Toshiyuki /add
 
+REM --- WiFi -------- https://blog.cles.jp/item/7660
 netsh wlan show profiles
-netsh wlan show profiles name=SSID
-netsh wlan show profiles name=SSID key=clear #key表示
-
-
-REM https://blog.cles.jp/item/7660
 netsh wlan show profiles name=HomeSSID key=clear
 netsh wlan export profile HomeSSID
 
 netsh wlan add profile filename="\\FILSVR\Share\Wi-Fi-HomeSSID.xml" user=all
 netsh wlan set profileparameter name=HomeSSID keymaterial=12345678abc
+
+REM -------------
+SET F=0
+if %COMPUTERNAME%==LENOVO-PC SET F=1
+if %COMPUTERNAME%==LENOVO-PC SET F=1
+if %COMPUTERNAME%==LENOVO-PC SET F=1
+if %F%==1 (
+   date /t
+)
