d90c76339e4b0a62681025ef71fb248349cc39e9
sdkn104 <sdkn104@yahoo.co.jp>
2018-03-03 16:39:29 +0900

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,11 +1,17 @@
 
 net use
 net use /PERSISTENT:NO
-net use Y: \\192.168.1.100\share\sadakane\HOKAN\‚»‚Ì‘¼
+net use Y: \\192.168.1.100\share\sadakane\HOKAN\ãã®ä»–
 net use
 dir Y:
 
 
 FOR /F "delims=^^^" %%i in ('ver') do set VVV=%%i
 echo %VVV%
+
+net user
+net user Toshiyuki
+net localgroup
+net localgroup Administrators
+net localgroup Administrators Toshiyuki /add
 
