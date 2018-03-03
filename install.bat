a2233e3a7229c9eeac4f4b644984d7932c53f0b3
sdkn104 <sdkn104@yahoo.co.jp>
2018-03-03 13:28:35 +0900

    Delete install.bat

--- install.bat
+++
@@ -1,6 +0,0 @@
-
-del  /F "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp\PCä«óùóp.vbs"
-copy /Y "%~dp0"PCä«óùóp.vbs "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp"
-
-pause
-
