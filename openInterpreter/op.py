38ddf02f00f6c5fb2fc40dc1c53c38de63eb3b6c
sdkn104 <sdkn104@gmail.com>
2025-03-10 09:42:23 +0900

    update op.py

---
+++ openInterpreter/op.py
@@ -0,0 +1,9 @@
+def init()
+  from interpreter import interpreter
+  import logging
+  import sys
+
+logging.basicConfig(stream=sys.stdout, encoding='UTF-8', level=logging.DEBUG, force=True)
+logging.basicConfig(level=logging.DEBUG)
+
+
