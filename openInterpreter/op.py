aaeef4b51e5265218f37a91719cc7bfdb3fa1c95
sdkn104 <sdkn104@gmail.com>
2025-03-10 18:14:48 +0900

    update op.py

--- openInterpreter/op.py
+++ openInterpreter/op.py
@@ -3,6 +3,6 @@ import logging
 import sys
 
 logging.basicConfig(stream=sys.stdout, encoding='UTF-8', level=logging.DEBUG, force=True)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, filename="drbug.txt", filemode="a")
 
 
