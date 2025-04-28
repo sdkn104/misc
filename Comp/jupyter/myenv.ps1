69b8fd85ff1165af964c6ad7d45db797f74b1732
sdkn104 <sdkn104@gmail.com>
2025-04-28 09:07:36 +0900

    update

--- Comp/jupyter/myenv.ps1
+++ Comp/jupyter/myenv.ps1
@@ -1,7 +1,8 @@
 
 
-set OPENAI_API_KEY=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
 set INTERPRETER_MODEL=gpt-4
+# set HTTP_PROXY=http://proxy.mei.melco.co.jp:9515
+# set HTTPS_PROXY=http://proxy.mei.melco.co.jp:9515
 
 Set-Location -Path $PSScriptRoot
 #Start-Process  ".\myenv\Scripts\Activate.ps1"
