f5648945147abd94ecca14eefe5677a8b54a1510
sdkn104 <sdkn104@gmail.com>
2025-05-05 10:29:47 +0900

    a

---
+++ Comp/OpenInterpreter/myenv.ps1
@@ -0,0 +1,14 @@
+
+
+set INTERPRETER_MODEL=gpt-4
+# set HTTP_PROXY=http://proxy.mei.melco.co.jp:9515
+# set HTTPS_PROXY=http://proxy.mei.melco.co.jp:9515
+
+Set-Location -Path $PSScriptRoot
+#Start-Process  ".\myenv\Scripts\Activate.ps1"
+
+& ./secret.ps1
+
+$env:PATH=$HOME + "\.cargo\bin;" + $env:PATH
+
+Start-Process powershell -ArgumentList '-NoExit', '-Command', '.\myenv\Scripts\Activate.ps1'
