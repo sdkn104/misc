23e2bb1ab31791918631dc1c3d2f90e2cd762f86
hashedtomato3 <hashedtomato3@gmail.com>
2026-02-22 18:56:10 +0900

    feat: add myenv.ps1 and test.json for environment setup and script configuration

---
+++ Comp/webserver/myenv.ps1
@@ -0,0 +1,11 @@
+
+
+set INTERPRETER_MODEL=gpt-4
+
+
+Set-Location -Path $PSScriptRoot
+#Start-Process  ".\myenv\Scripts\Activate.ps1"
+
+& ./secret.ps1
+
+Start-Process powershell -ArgumentList '-NoExit', '-Command', '.\myenv\Scripts\Activate.ps1'
